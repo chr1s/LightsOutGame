d89caf595dda30ea6eb6e31a7071ada742197c47
Chris Bates <16520455+chris-b@users.noreply.github.com>
2018-09-07 14:13:12 +0100

    Adds 5x5 game grid
    5x5 button grid added to form

--- LightsOutGame/Form1.cs
+++ LightsOutGame/Form1.cs
@@ -17,10 +17,32 @@ namespace LightsOutGame
         {
             InitializeComponent();
         }
-        
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            int gridColumn;
+            int gridRow;
 
+            // Buttons for game grid
+            Button[,] btn = new Button[5, 5];
+
+            // Create 5x5 grid of buttons
+            for (gridColumn = 0; gridColumn < 5; gridColumn++)
+            {
+                for (gridRow = 0; gridRow < 5; gridRow++)
+                {
+                    // Set button height/width/location
+                    btn[gridColumn, gridRow] = new Button
+                    {
+                        Height = 60,
+                        Width = 60,
+                        Location = new Point(60 * gridColumn, 60 * gridRow)
+                    };
+
+                    // Add button to form
+                    Controls.Add(btn[gridColumn, gridRow]);
+                }
+            }
         }
     }
 }
